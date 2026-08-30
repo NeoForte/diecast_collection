e609a7f632a8ac1b42fbb713d065a9fb7920b7fd
NeoForte <neoforte@protonmail.com>
2026-08-30 08:40:03 -0400

    Add files via upload
    3.3.4

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'pocket64-v3.3.2'
+const CACHE = 'pocket64-v3.3.4'
 const PRIVATE_PHOTO_CACHE_PREFIX = 'pocket64-private-photos-v2'
-const ASSETS = ['./', './index.html', './styles.css?v=3.3.2', './app.js?v=3.3.2', './showcase-sync.js?v=3.3.2', './manifest.webmanifest?v=3.3.2', './jszip.min.js?v=3.3.2', './black-brick-wall.svg', './icon-192.png', './icon-512.png', './pocket64-speedline-v251.png']
+const ASSETS = ['./', './index.html', './styles.css?v=3.3.4', './app.js?v=3.3.4', './showcase-sync.js?v=3.3.4', './manifest.webmanifest?v=3.3.4', './jszip.min.js?v=3.3.4', './black-brick-wall.svg', './icon-192.png', './icon-512.png', './pocket64-speedline-v251.png']
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)))
