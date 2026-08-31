c083e09639648a3fb731401fcd18fe7e552fbe0d
NeoForte <neoforte@protonmail.com>
2026-08-31 15:08:51 -0400

    Add files via upload
    4.0.4 fixed

--- showcase-sync.js
+++ showcase-sync.js
@@ -30,7 +30,7 @@
 
   function updateVisibleVersion() {
     document.querySelectorAll('.version-badge').forEach((badge) => {
-      if (badge.textContent !== 'Version 4.0.2') badge.textContent = 'Version 4.0.2'
+      if (badge.textContent !== 'Version 4.0.4') badge.textContent = 'Version 4.0.4'
     })
   }
 
