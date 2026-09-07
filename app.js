c5b954698661ac7654aa0012a89c64ae8c323c2f
NeoForte <neoforte@protonmail.com>
2026-09-07 18:35:35 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -3324,6 +3324,7 @@ async function saveCar() {
     }
 
     saveSetAssignment(car.id, pendingSetId, pendingSetPosition)
+    try { window.__p64CommunityPublish?.(car) } catch (hookError) { console.warn('Community Garage hook failed', hookError) }
     await loadCars()
     if (quickAddMode && !editingCar) {
       quickAddKeepBrand = $('diecast-brand').value
