ee4793a630e4f0332634a39e5d459cc08a3867f4
NeoForte <neoforte@protonmail.com>
2026-09-07 11:10:03 -0400

    Pocket 64 v6.1.3: remove obsolete version cleanup helper

--- p64-v606-cleanup.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  const FIX_VERSION = '6.0.9'
-  function syncDisplayedVersion() {
-    document.querySelectorAll('.version-badge').forEach((el) => { el.textContent = `Version ${FIX_VERSION}` })
-    document.documentElement.dataset.p64FixVersion = FIX_VERSION
-  }
-  if (document.readyState === 'loading') document.addEventListener('DOMContentLoaded', syncDisplayedVersion, { once:true })
-  else syncDisplayedVersion()
-})()
